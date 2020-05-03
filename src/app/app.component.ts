537cf7cde01c3ca4ee1cd949136804a4826a33c8
unknown <santoshmohan35@gmail.com>
2020-05-04 02:07:51 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Ecommerce';
+export class AppComponent implements OnInit {
+  title = 'Wishkart';
+
+  constructor() {}
+
+  ngOnInit() {}
 }
